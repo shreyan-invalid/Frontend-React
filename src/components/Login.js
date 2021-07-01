5d78247949172e641b9706001a670142ef9e292b
Swaraj  Pawar <pawarswaraj4@gmail.com>
2021-07-01 22:39:28 +0530

    Adding Recaptcha to Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,103 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import {Link} from 'react-router-dom';
+const SITE_KEY = "6LdFmGsbAAAAAP9O3oK2h9FLN5ldyEbHNvPes4L6";
 
 function Login() {
+
+    const [data, setdata] = useState({email:'',password:'',messageSent:false});
+
+    
+  
+    let name, value;
+    //Update changes in form
+    const handleChange = (e) => {
+        name = e.target.name;
+        value = e.target.value;
+        setdata({ ...data, [name]: value });
+    
+      };
+
+    //Google Recaptcha
+      useEffect(() => {
+        const loadScriptByURL = (id, url, callback) => {
+          const isScriptExist = document.getElementById(id);
+     
+          if (!isScriptExist) {
+            var script = document.createElement("script");
+            script.type = "text/javascript";
+            script.src = url;
+            script.id = id;
+            script.onload = function () {
+              if (callback) callback();
+            };
+            document.body.appendChild(script);
+          }
+     
+          if (isScriptExist && callback) callback();
+        }
+     
+        
+        loadScriptByURL("recaptcha-key", `https://www.google.com/recaptcha/api.js?render=${SITE_KEY}`, function () {
+          console.log("Script loaded!");
+        });
+      }, []);
+
+      const handleOnClick = e => {
+        e.preventDefault();
+        
+        window.grecaptcha.ready(() => {
+          window.grecaptcha.execute(SITE_KEY, { action: 'submit' }).then(token => {
+            submitData(token);
+          });
+        });
+      };
+
+
+      
+      // call a backend API to verify reCAPTCHA response
+
+      const submitData = token => {
+        fetch('http://localhost:4000/verify', {
+          method: 'POST',
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            
+            "email": data.email,
+            "password": data.password,
+            "g-recaptcha-response": token
+          })
+        }).then(res => res.json()).then(res => {
+          console.log(res.score); // we get the score here , so you can choose the minimum score you can allow
+
+          if(res.score>0.6){
+            //------
+            //send the email and password for passport authentication (make an API to backend)
+          }
+          else{
+              //-----
+              //dont allow entry to the user.
+          }
+         
+     
+        });
+      }
+
     return (
         <div>
-            <form action="/login" method="POST">
+            
+            <form >
                 <div>
                     <label for="email">Email</label>
-                    <input type="email" id="email" name="email" required/>
+                    <input type="email" id="email" name="email" value={data.email} onChange={handleChange} required/>
                 </div>
                 <div>
                     <label for="password">Password</label>
-                    <input type="password" id="password" name="password" required/>
+                    <input type="password" id="password" name="password" value={data.password} onChange={handleChange} required/>
                 </div>
-                <button type="submit">Login</button>
+                <button onClick={handleOnClick}>Login</button>
+                
             </form>
             <Link to="/register">Register</Link>
         </div>
